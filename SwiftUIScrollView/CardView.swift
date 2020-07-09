d32c98771742c31a4d1e29a8100401aaaaed4413
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-07-10 02:29:50 +0300

    Add CardView
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

---
+++ SwiftUIScrollView/CardView.swift
@@ -0,0 +1,21 @@
+//
+//  CardView.swift
+//  SwiftUIScrollView
+//
+//  Created by Abdelrahman Mohamed on 10.07.2020.
+//  Copyright © 2020 Abdelrahman Mohamed. All rights reserved.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardView()
+    }
+}
