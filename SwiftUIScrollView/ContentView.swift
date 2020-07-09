4a47af7b6021aa224eb27f8de338481fe33e8a80
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-07-10 02:25:49 +0300

    Initial Commit

---
+++ SwiftUIScrollView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIScrollView
+//
+//  Created by Abdelrahman Mohamed on 10.07.2020.
+//  Copyright © 2020 Abdelrahman Mohamed. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
