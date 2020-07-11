010aed75caee0bd57b2ebda2ac95b17ee449c957
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-07-12 01:15:17 +0300

    feat: Finalize Horizontal ScrollView and Vertical ScrollView
    BREAKING CHANGE: Finalize Horizontal ScrollView and Vertical ScrollView
    
     Chapter 5
     Understanding ScrollView and Building a Carousel UI
    
    Co-Authors: @Obadasemary

--- SwiftUIScrollView/ContentView.swift
+++ SwiftUIScrollView/ContentView.swift
@@ -10,7 +10,8 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ScrollView {
+
+        ScrollView(showsIndicators: false) {
 
             HStack {
                 VStack(alignment: .leading) {
@@ -19,15 +20,15 @@ struct ContentView: View {
                         .foregroundColor(.secondary)
 
                     Text("Your Reading")
-                        .font(.title)
+                        .font(.system(.largeTitle, design: .rounded))
                         .fontWeight(.black)
-                        .foregroundColor(.primary)
                 }
                 .layoutPriority(100)
 
                 Spacer()
             }
-            .padding()
+            .padding([.top, .horizontal])
+            .layoutPriority(-100)
 
 
             VStack {
@@ -38,6 +39,40 @@ struct ContentView: View {
                 CardView(image: "flutter-app", category: "Flutter", heading: "Building a Complex Layout with Flutter", author: "Lawrence Tan")
 
                 CardView(image: "natural-language-api", category: "iOS", heading: "What's New in Natural Language API", author: "Sai Kambampati")
+            }
+
+            ScrollView(.horizontal, showsIndicators: false) {
+
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("TODAY")
+                            .font(.caption)
+                            .foregroundColor(.secondary)
+
+                        Text("All Story")
+                            .font(.system(.largeTitle, design: .rounded))
+                            .fontWeight(.black)
+                    }
+                    .layoutPriority(100)
+
+                    Spacer()
+                }
+                .padding([.top, .horizontal])
+                .layoutPriority(-100)
+
+                HStack {
+                    CardView(image: "swiftui-button", category: "SwiftUI", heading: "Drawing a Border with Rounded Corners", author: "Abdelrahman Mohamed")
+                        .frame(width: 300)
+
+                    CardView(image: "macos-programming", category: "macOS", heading: "Building a Simple Editing App", author: "Gabriel Theodoropoulos")
+                        .frame(width: 300)
+
+                    CardView(image: "flutter-app", category: "Flutter", heading: "Building a Complex Layout with Flutter", author: "Lawrence Tan")
+                        .frame(width: 300)
+
+                    CardView(image: "natural-language-api", category: "iOS", heading: "What's New in Natural Language API", author: "Sai Kambampati")
+                        .frame(width: 300)
+                }
             }
         }
     }
