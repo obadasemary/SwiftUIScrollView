ef19bc03e43b98c7b19b94af0573d68326c75be8
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-07-12 00:28:42 +0300

    My Exercise Solution
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

--- SwiftUIScrollView/ContentView.swift
+++ SwiftUIScrollView/ContentView.swift
@@ -10,7 +10,36 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        ScrollView {
+
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("MONDAY, AUG 20")
+                        .font(.caption)
+                        .foregroundColor(.secondary)
+
+                    Text("Your Reading")
+                        .font(.title)
+                        .fontWeight(.black)
+                        .foregroundColor(.primary)
+                }
+                .layoutPriority(100)
+
+                Spacer()
+            }
+            .padding()
+
+
+            VStack {
+                CardView(image: "swiftui-button", category: "SwiftUI", heading: "Drawing a Border with Rounded Corners", author: "Abdelrahman Mohamed")
+
+                CardView(image: "macos-programming", category: "macOS", heading: "Building a Simple Editing App", author: "Gabriel Theodoropoulos")
+
+                CardView(image: "flutter-app", category: "Flutter", heading: "Building a Complex Layout with Flutter", author: "Lawrence Tan")
+
+                CardView(image: "natural-language-api", category: "iOS", heading: "What's New in Natural Language API", author: "Sai Kambampati")
+            }
+        }
     }
 }
 
